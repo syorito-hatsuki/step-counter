832f5bf7f174e1e4847ae0040f2d6ee22bee3cef
syorito-hatsuki <enderdron3@gmail.com>
2024-11-01 22:59:57 +0200

    Init project

---
+++ app/src/main/java/dev/syoritohatsuki/stepcounter/MainActivity.kt
@@ -0,0 +1,35 @@
+package dev.syoritohatsuki.stepcounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import dev.syoritohatsuki.stepcounter.ui.theme.StepCounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StepCounterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(modifier = Modifier.fillMaxSize(), text = "Step Counter")
+                    }
+                }
+            }
+        }
+    }
+}
