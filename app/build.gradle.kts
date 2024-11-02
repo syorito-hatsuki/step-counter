c23f81ccaec348429e36a4bf1626fc3d24b2e548
syorito-hatsuki <enderdron3@gmail.com>
2024-11-02 15:26:52 +0200

    Downgrade minSdk to 34 for test on real device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.syoritohatsuki.stepcounter"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
